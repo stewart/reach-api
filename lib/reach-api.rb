d5adc1734b09765ac14cf3b1e43c59f3afbcb55f
Andrew Stewart <andrew@averagestudios.com>
2012-01-27 10:51:57 -0800

    It tastes awful, but it works.

--- lib/reach-api.rb
+++ lib/reach-api.rb
@@ -1,7 +1,25 @@
-require "reach-api/version"
-
 module Reach
-  module Api
-    # Your code goes here...
+  class << self
+
+    # Alias for Reach::API.new
+    #
+    # Returns a Reach::API object
+    def new(options)
+      Reach::API.new(options)
+    end
+
+    # Delegate to Reach::API
+    def method_missing(method, *args, &block)
+      return super unless new.respond_to?(method)
+      new.send(method, *args, &block)
+    end
+
+    def respond_to?(method, include_private=false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
   end
+
+  autoload :API,     "reach/api"
+  autoload :Helper,  "reach/helper"
+  autoload :Version, "reach/version"
 end
